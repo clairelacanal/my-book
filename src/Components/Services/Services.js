2196075df5785411d6c0b42fa48266637b5bb5fb
Claire Lacanal <lacanal.claire@gmail.com>
2022-05-02 10:14:31 +0200

    services rectifications

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,12 +1,33 @@
 import React from "react";
 import './Services.css';
-import Graph from '../images/Graphic design basics.png';
+
 
 class Services extends React.Component{
     render(){
         return(
             <section id="services">
-                <img src={Graph} alt="présentation des services"/>
+                <div className="services-container">
+                    <h1>Mes services</h1>
+                    <div className="my-services">
+                        <div className="proposition-service">
+                            <h3>Création de votre site internet</h3>
+                            <p>Réalisation de A à Z à votre goût et à votre image</p>
+                        </div>
+                        <div className="proposition-service">
+                            <h3>Refonte de votre site web</h3>
+                            <p>Modernisation de votre site web préexistant</p>
+                        </div>
+                        <div className="proposition-service">
+                            <h3>Elaboration de vos contenus web</h3>
+                            <p>Création de contenus uniques et originaux qui répondent aux critères d'optimisation SEO</p>
+
+                        </div>
+                        <div className="proposition-service">
+                            <h3>Réalisation de newsletters</h3>
+                            <p>Elaboration et envoi afin de fidéliser les abonnés et en gagner de nouveaux</p>
+                        </div>
+                    </div>
+                </div>
             </section>
         )
     }
