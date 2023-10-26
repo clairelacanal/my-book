b32cba94c32e6adcb9413b2a374fec34e4694cc7
Claire Lacanal <lacanal.claire@gmail.com>
2023-10-26 15:04:28 +0200

    rectifications css

--- src/Components/BurgerMenu/BurgerMenu.js
+++ src/Components/BurgerMenu/BurgerMenu.js
@@ -12,7 +12,7 @@ const BurgerMenu = () => {
   return (
     <div className="burger-menu">
       <div className={`burger-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-        <div className="bar-bar"></div>
+        <div className="bar"></div>
         <div className="bar"></div>
         <div className="bar"></div>
       </div>
