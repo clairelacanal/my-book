1fb66e5da5114fcbac1ba3ffc10421d78c0b14ee
Claire Lacanal <lacanal.claire@gmail.com>
2023-10-26 10:10:08 +0200

    rectification navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Accueil from './Components/Accueil/Accueil';
 import About from './Components/About/About';
@@ -7,22 +8,41 @@ import MesProjets from './Components/MesProjets/MesProjets';
 import Baniere from './Components/Banniere/Banniere';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
+import BurgerMenu from './Components/BurgerMenu/BurgerMenu';
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // Met à jour la largeur de la fenêtre lorsque la fenêtre est redimensionnée
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const isMobile = windowWidth <= 768;
+  const isTablet = windowWidth > 768 && windowWidth <= 1024;
+
   return (
     <div className="App">
       <header>
-        <Navbar/>
+        {isMobile ? <BurgerMenu /> : <Navbar className={isTablet ? 'tablet-navbar' : ''} />}
       </header>
       <main>
-        <Accueil/>
-        <About/>
-        <Services/>
-        <MesProjets/>
-        <Baniere/>
-        <Contact/>
+        <Accueil />
+        <About />
+        <Services />
+        <MesProjets />
+        <Baniere />
+        <Contact />
       </main>
-        <Footer/>
+      <Footer />
     </div>
   );
 }
