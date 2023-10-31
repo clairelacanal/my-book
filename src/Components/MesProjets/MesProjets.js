c72b009f822c2ea5e1a0df3fdafc6a5acf7c22b6
Claire Lacanal <lacanal.claire@gmail.com>
2023-10-31 10:13:21 +0100

    projets section done

--- src/Components/MesProjets/MesProjets.js
+++ src/Components/MesProjets/MesProjets.js
@@ -1,8 +1,6 @@
 import React from "react";
 import './MesProjets.css';
-import food from '../images/foods.jpg';
-import plants from '../images/plants.jpg';
-import child from '../images/toys.jpg';
+
 
 class MesProjets extends React.Component {
     render(){
@@ -12,13 +10,13 @@ class MesProjets extends React.Component {
                 <p>Dernières réalisations</p>
                 <div className="projets-background">
                     <article className="app-react-food">
-                        <button><a href="https://basil-recipes.herokuapp.com/" target="_blank" rel="noopener noreferrer">BASIL l'appli anti-gaspi</a></button>
+                        <button><a href="https://basil-recipes.herokuapp.com/" target="_blank" rel="noopener noreferrer">BASIL</a></button>
                     </article>
                     <article className="app-react-plant">
-                        <button><a href="https://plant-sitter.herokuapp.com/" target="_blank" rel="noopener noreferrer">PLANT-SITTER, gardiennage de plantes vertes</a></button>
+                        <button><a href="https://plant-sitter.herokuapp.com/" target="_blank" rel="noopener noreferrer">PLANT-SITTER</a></button>
                     </article>
                     <article className="app-react-kids">
-                        <button><a href="https://plant-sitter.herokuapp.com/" target="_blank" rel="noopener noreferrer">NOUNOURS A FAIM, en construction</a></button>
+                        <button><a href="https://plant-sitter.herokuapp.com/" target="_blank" rel="noopener noreferrer">NOUNOURS A FAIM</a></button>
                     </article>
                 </div>
             </section>
