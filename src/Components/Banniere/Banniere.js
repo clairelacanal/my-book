1e2a7e2c60f13a73fada3002f9e33ba72d4f2179
Claire Lacanal <lacanal.claire@gmail.com>
2023-10-31 11:53:06 +0100

    creation popup composant

--- src/Components/Banniere/Banniere.js
+++ src/Components/Banniere/Banniere.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import './Banniere.css';
+import ObtenirUnDevisForm from "../Devis/ObtenirUnDevisForm";
 
-class Baniere extends React.Component{
-    render(){
-        return(
-            <section className="banniere">
-                <div className="background-banniere">
-                    <p>Avez-vous une idée de projet ?</p>
-                    <h1>Travaillons ensemble efficacement !</h1>
-                    <button>Obtenir un devis</button>
-                </div>
-            </section>
-        )
-    }
+class Baniere extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      afficherDevisPopup: false
+    };
+  }
+
+  afficherDevisPopup = () => {
+    this.setState({ afficherDevisPopup: true });
+  };
+
+  fermerDevisPopup = () => {
+    this.setState({ afficherDevisPopup: false });
+  };
+
+  render() {
+    const { afficherDevisPopup } = this.state;
+
+    return (
+      <section className="banniere">
+        <div className="background-banniere">
+          <p>Avez-vous une idée de projet ?</p>
+          <h1>Travaillons ensemble efficacement !</h1>
+          <button onClick={this.afficherDevisPopup}>Obtenir un devis</button>
+          {afficherDevisPopup && (
+            <div className="devis-popup">
+              <div className="devis-popup-contenu">
+                <span className="fermer" onClick={this.fermerDevisPopup}>
+                  &times;
+                </span>
+                <ObtenirUnDevisForm/>
+              </div>
+            </div>
+          )}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Baniere;
+
---
+++ src/Components/Devis/ObtenirUnDevisForm.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import './ObtenirUnDevisForm.css';
+
+function ObtenirUnDevisForm() {
+  // Utilisation du useState hook pour gérer les états des champs du formulaire
+  const [nom, setNom] = useState('');
+  const [prenom, setPrenom] = useState('');
+  const [email, setEmail] = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [message, setMessage] = useState('');
+
+  // Fonction pour gérer la soumission du formulaire
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Vous pouvez envoyer les données du formulaire à votre serveur ou effectuer toute autre action nécessaire ici
+    console.log('Données du formulaire :', { nom, prenom, email, telephone, message });
+
+    // Réinitialisation des champs du formulaire après la soumission si nécessaire
+    setNom('');
+    setPrenom('');
+    setEmail('');
+    setTelephone('');
+    setMessage('');
+  };
+
+  return (
+    <div className='overlay'>
+      <div className='popup'>
+      <h2>Obtenir un devis</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="nom">Nom :</label>
+          <input
+            type="text"
+            id="nom"
+            value={nom}
+            onChange={(e) => setNom(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="prenom">Prénom :</label>
+          <input
+            type="text"
+            id="prenom"
+            value={prenom}
+            onChange={(e) => setPrenom(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email :</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="telephone">Téléphone :</label>
+          <input
+            type="tel"
+            id="telephone"
+            value={telephone}
+            onChange={(e) => setTelephone(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message :</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Envoyer</button>
+      </form>
+    </div>
+    </div>
+    
+  );
+}
+
+export default ObtenirUnDevisForm;
