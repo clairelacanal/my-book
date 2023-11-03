8a77fa9742ed8b00d444a195a852ba3c1e4cb3cb
Claire Lacanal <lacanal.claire@gmail.com>
2023-11-03 15:36:08 +0100

    popup v3 on going + button close popup done

--- src/Components/Devis/Confirmation.js
+++ src/Components/Devis/Confirmation.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Confirmation.css';
 
 function Confirmation() {
   return (
-    <div>
-      <h2>Merci. Votre demande sera traitée.</h2>
+    <div className="container-confirmation">
+      <div className='container-confirmation-icon'>
+      <span className="confirmation-icon"></span>
+      </div>
+      <div className='container-paragraphe-confirmation'>
+        <p>Merci de votre confiance.</p>
+        <p>Votre demande sera traitée dans les plus brefs délais.</p>
+      </div>
     </div>
   );
 }
--- src/Components/Devis/ObtenirUnDevisForm.js
+++ src/Components/Devis/ObtenirUnDevisForm.js
@@ -4,24 +4,22 @@ import './Confirmation';
 import Confirmation from './Confirmation';
 
 function ObtenirUnDevisForm() {
-  // Utilisation du useState hook pour gérer les états des champs du formulaire
   const [demandeSoumise, setDemandeSoumise] = useState(false);
+  const [afficherTitre, setAfficherTitre] = useState(true);
+  const [afficherPopup, setAfficherPopup] = useState(true);
   const [nom, setNom] = useState('');
   const [prenom, setPrenom] = useState('');
   const [email, setEmail] = useState('');
   const [telephone, setTelephone] = useState('');
   const [message, setMessage] = useState('');
-  
 
-  // Fonction pour gérer la soumission du formulaire
   const handleSubmit = (e) => {
     e.preventDefault();
     setDemandeSoumise(true);
+    setAfficherTitre(false);
+    // Laisser l'affichage du popup activé pour afficher la confirmation
 
-    // Envoyer les données du formulaire à votre serveur ou effectuer toute autre action nécessaire ici
     console.log('Données du formulaire :', { nom, prenom, email, telephone, message });
-
-    // Réinitialisation des champs du formulaire après la soumission si nécessaire
     setNom('');
     setPrenom('');
     setEmail('');
@@ -29,75 +27,93 @@ function ObtenirUnDevisForm() {
     setMessage('');
   };
 
+  const handleClose = () => {
+    setDemandeSoumise(false);
+    setAfficherTitre(true);
+    setAfficherPopup(false); // Désactiver l'affichage du popup
+  };
+
+  const fermerPopup = () => {
+    handleClose();
+  };
+
   return (
-    <div className='overlay'>
-      <div className='popup'>
-      <h2>Obtenir un devis</h2>
-      {!demandeSoumise ? (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="nom">Nom<span className="required">*</span> :</label>
-          <input
-            type="text"
-            id="nom"
-            value={nom}
-            onChange={(e) => setNom(e.target.value)}
-            required
-            className="input-field"
-          />
+    <>
+      {afficherPopup && (
+        <div className='overlay' onClick={fermerPopup}>
+          <div className='popup' onClick={(e) => e.stopPropagation()}>
+            {afficherTitre && <h2>Obtenir un devis</h2>}
+            {!demandeSoumise ? (
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="nom">Nom<span className="required">*</span> :</label>
+                    <input
+                    type="text"
+                    id="nom"
+                    value={nom}
+                    onChange={(e) => setNom(e.target.value)}
+                    required
+                    className="input-field"
+                    />
+                </div>
+                <div>
+                  <label htmlFor="prenom">Prénom<span className="required">*</span> :</label>
+                    <input
+                    type="text"
+                    id="prenom"
+                    value={prenom}
+                    onChange={(e) => setPrenom(e.target.value)}
+                    required
+                    className="input-field"
+                    />
+                </div>
+                <div>
+                  <label htmlFor="email">Email<span className="required">*</span> :</label>
+                    <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="input-field"
+                    />
+                </div>
+                <div>
+                  <label htmlFor="telephone">Téléphone<span className="required">*</span> :</label>
+                    <input
+                    type="tel"
+                    id="telephone"
+                    value={telephone}
+                    onChange={(e) => setTelephone(e.target.value)}
+                    required
+                    className="input-field"
+                    />
+                </div>
+                <div>
+                  <label htmlFor="message">Message<span className="required">*</span> :</label>
+                    <textarea
+                    id="message"
+                    value={message}
+                    placeholder='Votre projet...'
+                    onChange={(e) => setMessage(e.target.value)}
+                    required
+                    className="input-field"
+                    />
+                </div>
+                <button type="submit" className='submit-devis'>Envoyer</button>
+              </form>
+              ) : (
+                <div>
+                  <Confirmation />
+                  <button onClick={fermerPopup} className="close-button">
+                    Fermer
+                  </button>
+                </div>
+              )}
+          </div>
         </div>
-        <div>
-          <label htmlFor="prenom">Prénom<span className="required">*</span> :</label>
-          <input
-            type="text"
-            id="prenom"
-            value={prenom}
-            onChange={(e) => setPrenom(e.target.value)}
-            required
-            className="input-field"
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email<span className="required">*</span> :</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="input-field"
-          />
-        </div>
-        <div>
-          <label htmlFor="telephone">Téléphone<span className="required">*</span> :</label>
-          <input
-            type="tel"
-            id="telephone"
-            value={telephone}
-            onChange={(e) => setTelephone(e.target.value)}
-            required
-            className="input-field"
-          />
-        </div>
-        <div>
-          <label htmlFor="message">Message<span className="required">*</span> :</label>
-          <textarea
-            id="message"
-            value={message}
-            placeholder='Votre projet...'
-            onChange={(e) => setMessage(e.target.value)}
-            required
-            className="input-field"
-          />
-        </div>
-        <button type="submit" className='submit-devis'>Envoyer</button>
-      </form>
-      ) : (
-        <Confirmation/>
       )}
-    </div>
-    </div>
-    
+    </>
   );
 }
 
