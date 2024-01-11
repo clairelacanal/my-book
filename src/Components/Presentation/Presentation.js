80a7362b17f09134b1126063d3a55325969b8ce6
Claire Lacanal <lacanal.claire@gmail.com>
2024-01-11 14:15:37 +0100

    rectification presentation

--- src/Components/Presentation/Presentation.js
+++ src/Components/Presentation/Presentation.js
@@ -7,8 +7,8 @@ class Presentation extends React.Component {
             <section className="presentation-me">
                 <h1>Hello !</h1>
                 <h2>Je suis <span>Claire LACANAL</span></h2>
-                <p>Développeuse web, je vous propose mes services afin de promouvoir vos activités sur la toile.</p>
-                <p>De la charte graphique, en passant par la réalisation et le conseil, je vous accompagne dans vos projets digitaux.</p>
+                <p>Je recherche une entreprise pour effectuer mon alternance, en qualité de développeuse Front-End ou Full-Stack, sur une période de 12 mois. </p>
+                <p>Motivée et prête à apprendre vos technos, je suis la candidate idéale pour vos projets digitaux !</p>
             </section>
         )
     }
